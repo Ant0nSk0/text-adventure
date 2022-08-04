e7beb3fc23f78818d1e6a4557275d983f0a11c15
Ant0nSk0 <anton.skovlund@gmail.com>
2022-08-04 14:31:30 +0000

    Add start of act1

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ def new_game():
         name = input("Please enter your name:\n")
         global p1
         p1 = Player(name, False, False)
+        act1()
     elif answer.lower() == "no":
         print("Shame. Maybe another time. Good bye.")
     else:
@@ -26,4 +27,24 @@ def new_game():
         new_game()
 
 
+def act1():
+    """
+    The first act of the game
+    """
+    print("Leaving the safety of your home, your journey begins")
+    print("You enter a dark forest, struggling to see the path before you")
+    print("Suddenly, a voice cries out from the dark")
+    print(f"..is that.. ..is that you {p1.name}?\n")
+    answer = input("Do you answer the voice? [yes/no]\n")
+    if answer.lower() == "yes":
+        print("You answer the voice and get nothing but silence in return")
+    elif answer.lower() == "no":
+        print("You ignore the voice and keep walking")
+        print("Suddenly the air grows cold and your body frozen in place.")
+    else:
+        print("Please enter either yes or no")
+        act1()
+
+
+# run game
 new_game()

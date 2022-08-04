f1a2ef88710d82417ba76676643888cd35b97ba8
Ant0nSk0 <anton.skovlund@gmail.com>
2022-08-04 13:56:20 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Player:
+    """
+    Store player name and pick ups
+    """
+    def __init__(self, name, griff, lucky_dice):
+        self.name = name
+        self.griff = griff
+        self.lucky_dice = lucky_dice
+
+
+def new_game():
+    """
+    Starts the game
+    """
+    print("Start new game?")
+    answer = input("[yes/no]\n")
+    if answer.lower() == "yes":
+        print("Great! Let us begin!")
+        name = input("Please enter your name:\n")
+        global p1
+        p1 = Player(name, False, False)
+    elif answer.lower() == "no":
+        print("Shame. Maybe another time. Good bye.")
+    else:
+        print("Please enter either yes or no")
+        new_game()
+
+
+new_game()
